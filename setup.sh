3194059c7c18bde6ab6543bbbf84f20a7520f95a
Alice <alisiamsk@gmail.com>
2025-05-20 12:07:02 +0300

    Initial commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-cp flask_app.nginx.conf /etc/nginx/sites-enabled/flask_app.nginx.conf
+cp flask_app.nginx.conf /etc/nginx/sites-enabled/felv.nginx.conf
 certbot --nginx -d felv.silaeder.codingprojects.ru
